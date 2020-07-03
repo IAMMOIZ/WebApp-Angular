5d6e9431a1f1191eb6aac47334a584e5d4f030e9
IAMMOIZ <moizmit02@gmail.com>
2020-07-03 23:06:52 +0530

    error resolving in app component and app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AppComponent} from './app.component';
 @NgModule({
-declartion : [AppComponent],
+declarations : [AppComponent],
 providers : [],
 imports : [BrowserModule],
 bootstrap : [AppComponent]
